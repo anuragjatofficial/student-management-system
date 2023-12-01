e658896e323c231d92e5d80038c9c768cf9c3d26
anuragjatofficial <anurag.1101140@gmail.com>
2023-12-01 20:54:29 +0530

    added fronted files

---
+++ frontend/student-management/src/App.tsx
@@ -0,0 +1,12 @@
+import LoginPage from "./components/LoginPage"
+
+export const App = () => {
+  
+
+
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+}
---
+++ frontend/student-management/src/components/LoginPage.tsx
@@ -0,0 +1,248 @@
+import React, { FormEvent, useState } from 'react'
+import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import axios from 'axios';
+
+export default function LoginPage() {
+  const [firstName,setFirstName] = useState("");
+  const [lastName,setLastName] = useState("");
+  const [dob,setDob] = useState("");
+  const [gender,setGender] = useState("MALE");
+  const [phone,setPhone] = useState(""); 
+  const [streetAddress,setStreetAddress] = useState("");
+  const [city,setCity] =useState("");
+  const [province,setProvince] = useState("");
+  const [zip,setZip] = useState("");
+  const baseURL = process.env.REACT_APP_API_BASE_URL;
+
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const Student = {
+      firstName: firstName,
+      lastName: lastName,
+      dob: dob,
+      gender: gender,
+      phone: phone,
+      address: `${streetAddress} ${city} ${province}- ${zip}`,
+    };
+
+    axios.post(`${baseURL}/students`, Student)
+          .then((res)=>{
+            console.log(res);
+            
+          })
+  };
+
+
+  return (
+    <div className="w-full flex items-center justify-center pt-4 ">
+      <form
+        className="w-[40%] shadow-lg  px-10 py-10"
+        onSubmit={(e) => {handleLogin(e)}}
+      >
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Student Information
+            </h2>
+            {/* <p className="mt-1 text-sm leading-6 text-gray-600">
+              Use a permanent address where you can receive mail.
+            </p> */}
+
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="first-name"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  First name
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autoComplete="given-name"
+                    placeholder="John"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="last-name"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Last name
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    autoComplete="family-name"
+                    placeholder="doe"
+                    onChange={(e) => setLastName(e.target.value)}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="phone"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Phone
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="phone"
+                    type="text"
+                    autoComplete="email"
+                    onChange={(e) => setPhone(e.target.value)}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="gender"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Gender
+                </label>
+                <div className="mt-2">
+                  <select
+                    id="country"
+                    name="gender"
+                    autoComplete="country-name"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                    onChange={(e) => setGender(e.target.value)}
+                  >
+                    <option>MALE</option>
+                    <option>FEMALE</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="dob"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Date of birth
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="date"
+                    name="dob"
+                    id="dob"
+                    autoComplete="family-name"
+                    placeholder="doe"
+                    onChange={(e) => setDob(e.target.value)}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="col-span-full">
+                <label
+                  htmlFor="street-address"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Street address
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="street-address"
+                    id="street-address"
+                    autoComplete="street-address"
+                    onChange={(e) => setStreetAddress(e.target.value)}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2 sm:col-start-1">
+                <label
+                  htmlFor="city"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  City
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="city"
+                    id="city"
+                    autoComplete="address-level2"
+                    onChange={(e) => setCity(e.target.value)}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="region"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  State / Province
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="region"
+                    id="region"
+                    autoComplete="address-level1"
+                    onChange={(e) => setProvince(e.target.value)}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="postal-code"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  ZIP / Postal code
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="number"
+                    name="postal-code"
+                    id="postal-code"
+                    autoComplete="postal-code"
+                    onChange={(e) => setZip(e.target.value)}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <button
+            type="button"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Add Student
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
